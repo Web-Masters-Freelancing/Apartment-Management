6d6700c135732091cb60bc93ae9b2a542cef48be
Dexter Louie Aniez <dexterlouie.aniez@sun-asterisk.com>
2024-01-12 20:25:31 +0800

    [Task][BE] API to create room

--- server/src/room/room.module.ts
+++ server/src/room/room.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { RoomService } from './room.service';
 import { PrismaService } from '../prisma/prisma.service';
+import { RoomController } from './room.controller';
 
 @Module({
   providers: [RoomService, PrismaService],
   exports: [RoomService],
+  controllers: [RoomController],
 })
 export class RoomModule {}
