e66edf98b170be14cc1ad0ec7ae68c3523302765
Al Oletres <alejandrooletres@gmail.com>
2024-02-10 17:58:18 +0800

    [Task][FE] Create password reset page

--- web/src/app/settings/page.tsx
+++ web/src/app/settings/page.tsx
@@ -1,12 +1,104 @@
-import { Box, Typography } from "@mui/material";
+"use client";
+import {
+  Typography,
+  Card,
+  CardContent,
+  CardMedia,
+  CardActions,
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Collapse,
+  Box,
+  Button,
+} from "@mui/material";
 import WrapperLayout from "@/app/wrapper.layout";
+import { Key, ExpandMore, ExpandLess } from "@mui/icons-material";
+import { useHook } from "./hooks";
+import CustomInput from "@/components/Input";
+import { Form, Formik } from "formik";
 
 const SettingsPage = () => {
+  const { handleSubmit, handleClick, open } = useHook();
   return (
     <WrapperLayout>
-      <Box>
-        <Typography variant="h6">Settings works </Typography>
-      </Box>
+      <Card variant="outlined">
+        <CardMedia
+          sx={{ height: 140 }}
+          component="img"
+          image="./../assets/images.png"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Account Security
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Take actions to make your account more secure.
+          </Typography>
+          <List>
+            <ListItemButton onClick={handleClick}>
+              <ListItemIcon>
+                <Key />
+              </ListItemIcon>
+              <ListItemText primary="Change your password" />
+              {open ? <ExpandMore /> : <ExpandLess />}
+            </ListItemButton>
+
+            <Collapse in={open} timeout="auto" unmountOnExit>
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "center",
+                  width: "100%",
+                  gap: 1,
+                }}
+              >
+                <Formik
+                  initialValues={{ password: "", newPassword: "" }}
+                  onSubmit={handleSubmit}
+                >
+                  {({ submitForm, isSubmitting }) => (
+                    <Form>
+                      <CustomInput
+                        label="Current password"
+                        name="password"
+                        id="password"
+                        type="password"
+                        margin="dense"
+                      />
+                      <CustomInput
+                        label="New password"
+                        name="newPassword"
+                        id="newPassword"
+                        type="password"
+                        margin="dense"
+                      />
+
+                      <Box
+                        sx={{
+                          display: "flex",
+                          paddingTop: 1,
+                          justifyContent: "end",
+                        }}
+                      >
+                        <Button
+                          variant="contained"
+                          onSubmit={submitForm}
+                          disabled={isSubmitting}
+                        >
+                          Save changes
+                        </Button>
+                      </Box>
+                    </Form>
+                  )}
+                </Formik>
+              </Box>
+            </Collapse>
+          </List>
+        </CardContent>
+        <CardActions></CardActions>
+      </Card>
     </WrapperLayout>
   );
 };
--- web/src/components/Navbar.tsx
+++ web/src/components/Navbar.tsx
@@ -1,11 +1,25 @@
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import { AppBar, Box, Toolbar, Typography, Tooltip } from "@mui/material";
+import LogoutIcon from "@mui/icons-material/Logout";
 
 const Navbar = () => {
   return (
     <div>
       <AppBar sx={{ background: "#000" }} position="static">
-        <Toolbar>
+        <Toolbar
+          sx={{
+            display: "flex",
+            width: "100%",
+            justifyContent: "space-between",
+          }}
+        >
           <Typography variant="h6">Whitehouse Apartment Management</Typography>
+          <Tooltip title="Log out" arrow>
+            <LogoutIcon
+              sx={{
+                ":hover": { color: "blue", cursor: "pointer" },
+              }}
+            />
+          </Tooltip>
         </Toolbar>
       </AppBar>
     </div>
